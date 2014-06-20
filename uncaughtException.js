4626e3950f68899eeee0adb635051f02667d0f08
Itai Koren <itkoren@gmail.com>
2014-06-20 14:53:35 +0300

    add example for uncughException event
    Signed-off-by: Itai Koren <itkoren@gmail.com>

---
+++ uncaughtException.js
@@ -0,0 +1,24 @@
+// Include The 'http' Module
+var http = require("http");
+
+// Create the HTTP Server
+var server = http.createServer(function(req, res) {
+    // Handle HTTP Request
+    var result = decodeURIComponent(req.url);
+
+    // chop off / at the beginning %
+    result = result.slice(1);
+    result = result.toUpperCase(); // SHOUT
+
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(result + "\n");
+}).listen(process.env.PORT || 8000, process.env.HOST || "0.0.0.0", function() {
+    console.log("HTTP Server Started. Listening on " + server.address().address + " : Port " + server.address().port);
+});
+
+process.on("uncaughtException", function (err) {
+    console.error((new Date()).toUTCString() + " uncaughtException:", err.message);
+    console.error(err.stack);
+
+    process.exit(1);
+});
